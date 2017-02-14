0b00c0581baa2b8887bfdb3c9e9ca987ec44b08f
SAUL-PC\Saúl <saulete_1990@hotmail.com>
2017-02-14 08:49:43 +0100

    Primera prueba

---
+++ basketGo-/basketGO/src/main/java/es/sidelab/urjc/JugadorController.java
@@ -0,0 +1,37 @@
+package es.sidelab.urjc;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class JugadorController {
+	
+
+	@Autowired
+	private JugadorRepository jugador;
+	
+	@PostConstruct
+	public void init(){
+	}
+	
+	@RequestMapping("/jugadores/")
+	public Page<Jugador> todosJugadores(Pageable page){
+		
+		return jugador.findAll(page);
+		
+	}
+	
+	@RequestMapping("/jugadores/porNombre/")
+	public Page<Jugador> nombreJugadores(String nombre, Pageable page){
+		
+		return jugador.findByNombre(nombre, page);
+		
+	}
+
+}
---
+++ basketGo-/basketGO/src/test/java/es/sidelab/urjc/BasketGoApplicationTests.java
@@ -0,0 +1,16 @@
+package es.sidelab.urjc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasketGoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
